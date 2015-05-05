9a591cd16464fff4f2617afb66f5ce536bbf63e1
mambax7 <mambax7@gmail.com>
2015-05-05 09:31:49 -0400

    Original files

---
+++ studio/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="BCOOL THEME STUDIO"
+Depends=Legacy_RenderSystem
+Url="http://2bcool.net/"
+Version="1.3"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=M.Taka BCOOL Inc.
+ScreenShot="screenshot.png"
+Description="Good theme for photo lovers."
+W3C=
+
+License="Attribution-NonCommercial-ShareAlike 2.1 Japan"
+
+*/
+?>
